af7bab69622ee941129430c06060c4b87b8d26e9
Joseph Luck <joseph.luck@sky.com>
2021-01-11 09:16:58 +0000

    Create util for computing paths through to a state

---
+++ machi/src/pathways.spec.ts
@@ -0,0 +1,167 @@
+import {
+  generateChart,
+  generateChartFromLinks,
+} from "./graph/generate-mermaid-chart";
+import { State } from "./machine";
+import { extractFromNamesFromLinks, getPathwaysToState } from "./pathways";
+
+/**
+ * It's worthwhile looking at the generated charts in screenshots/tests/ to see
+ * what these machines look like. It'll make reading the tests a little easier.
+ */
+
+describe("Possible paths through a machine that uses exclusive forks", () => {
+  const states: State<any, any, any>[] = [
+    {
+      id: "E1",
+      isDone: [() => true],
+    },
+    {
+      fork: "F1",
+      requirements: [() => true],
+      states: [
+        {
+          id: "E2",
+          isDone: [() => true],
+        },
+        {
+          fork: "F2",
+          requirements: [() => true],
+          states: [
+            {
+              id: "E7",
+              isDone: [],
+            },
+          ],
+        },
+        {
+          id: "E3",
+          isDone: [() => true],
+        },
+      ],
+    },
+    {
+      fork: "F1",
+      requirements: [() => true],
+      states: [
+        {
+          id: "E4",
+          isDone: [() => true],
+        },
+        {
+          id: "E5",
+          isDone: [() => true],
+        },
+      ],
+    },
+    {
+      id: "E6",
+      isDone: [() => true],
+    },
+  ];
+
+  it("generates the chart (for debugging purposes)", async () => {
+    await generateChart(
+      { output: "../screenshots/tests/pathways-machine.png" },
+      states
+    );
+  });
+
+  it("gets possible paths to F1", () => {
+    const result = getPathwaysToState("F1", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual([["E1"]]);
+  });
+
+  it("gets possible paths to E2", () => {
+    const result = getPathwaysToState("E2", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual([["E1", "F1"]]);
+  });
+
+  it("gets possible paths to E6", () => {
+    const result = getPathwaysToState("E6", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual(
+      expect.arrayContaining([
+        ["E1", "F1"],
+        ["E1", "F1", "E2", "F2", "E3"],
+        ["E1", "F1", "E4", "E5"],
+      ])
+    );
+  });
+
+  it("gets possible paths to E7", () => {
+    const result = getPathwaysToState("E7", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual(
+      expect.arrayContaining([["E1", "F1", "E2", "F2"]])
+    );
+  });
+
+  it("generates charts for possible pathways", async () => {
+    const result = getPathwaysToState("E6", states);
+    await Promise.all(
+      result.map((result, i) =>
+        generateChartFromLinks(
+          {
+            output: `../screenshots/tests/pathways-machine-mapped-result-${
+              i + 1
+            }.png`,
+          },
+          result
+        )
+      )
+    );
+  });
+});
+
+describe("Generates possible paths through a machine that uses skipped forks", () => {
+  const states: State<{}, {}, {}>[] = [
+    {
+      id: "E1",
+      isDone: [() => true],
+    },
+    {
+      fork: "F1",
+      requirements: [() => true],
+      states: [
+        {
+          id: "E2",
+          isDone: [() => true],
+        },
+      ],
+    },
+    {
+      id: "E3",
+      isDone: [() => true],
+    },
+  ];
+
+  it("generates the chart (for debugging purposes)", async () => {
+    await generateChart(
+      { output: "../screenshots/tests/pathways-machine-2.png" },
+      states
+    );
+  });
+
+  it("gets possible paths to F1", () => {
+    const result = getPathwaysToState("F1", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual(
+      expect.arrayContaining([["E1"]])
+    );
+  });
+
+  it("gets possible paths to E2", () => {
+    const result = getPathwaysToState("E2", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual(
+      expect.arrayContaining([["E1", "F1"]])
+    );
+  });
+
+  it("gets possible paths to E3", () => {
+    const result = getPathwaysToState("E3", states);
+    expect(result.map(extractFromNamesFromLinks)).toEqual(
+      expect.arrayContaining([
+        ["E1", "F1", "E2"],
+        ["E1", "F1"],
+      ])
+    );
+  });
+});
---
+++ machi/src/pathways.ts
@@ -0,0 +1,60 @@
+import { generateStateLinks } from "./graph/generate-state-links";
+import { StateLink } from "./graph/utils";
+import { Condition, isFork, State } from "./machine";
+
+export const getPathwaysToState = <
+  Context,
+  Conditions extends { [key: string]: Condition<Context> },
+  AdditionalEntryData
+>(
+  toName: string,
+  states: State<Context, Conditions, AdditionalEntryData>[],
+  links = generateStateLinks(states).filter(
+    (link) => link.type === "link"
+  ) as StateLink<Context, Conditions, AdditionalEntryData>[]
+): StateLink<Context, Conditions, AdditionalEntryData>[][] => {
+  const branches = findLinksToState(toName, links);
+
+  if (branches.length === 0) {
+    return [];
+  }
+
+  return branches.map((branch) => {
+    const ways = getPathwaysToState(extractFromNameFromLink(branch), states);
+
+    if (ways.length) {
+      return ways.reduce((acc, way) => [...way, ...acc, branch], []);
+    }
+
+    return [branch];
+  });
+};
+
+const findLinksToState = <
+  Context,
+  Conditions extends { [key: string]: Condition<Context> },
+  AdditionalEntryData
+>(
+  name: string,
+  links: StateLink<Context, Conditions, AdditionalEntryData>[]
+): StateLink<Context, Conditions, AdditionalEntryData>[] =>
+  links.filter((link) =>
+    isFork(link.to) ? link.to.fork === name : link.to.id === name
+  );
+
+export const extractFromNamesFromLinks = <
+  Context,
+  Conditions extends { [key: string]: Condition<Context> },
+  AdditionalEntryData
+>(
+  links: StateLink<Context, Conditions, AdditionalEntryData>[]
+): string[] => links.map(extractFromNameFromLink);
+
+const extractFromNameFromLink = <
+  Context,
+  Conditions extends { [key: string]: Condition<Context> },
+  AdditionalEntryData
+>(
+  link: StateLink<Context, Conditions, AdditionalEntryData>
+): string =>
+  isFork(link.from) ? link.from.fork : link.from ? link.from.id : "unknown";
